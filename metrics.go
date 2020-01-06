92874469a5bd6f557c882f486e276c2cdd080c5b
Marc Denman <marc.denman@ukho.gov.uk>
2020-01-06 16:45:37 +0000

    Add histograms for jobs

--- metrics.go
+++ metrics.go
@@ -43,6 +43,50 @@ var (
 	)
 )
 
+func calculateHistograms(metricContext metricsContext) []prometheus.Metric {
+
+	totalTimes := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:        "tfs_pool_job_total_length_secs",
+		Help:        "Total length of job duration for pool",
+		Buckets:     prometheus.LinearBuckets(0, 60, 20), // 40 buckets, each 30 seconds wide
+		ConstLabels: map[string]string{"pool": metricContext.pool.Name},
+	})
+
+	for _, job := range metricContext.finishedJobs {
+		totalTime := job.FinishTime.Sub(job.QueueTime)
+		totalTimes.Observe(totalTime.Seconds())
+	}
+
+	queueTimes := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:        "tfs_pool_job_queue_length_secs",
+		Help:        "Total length of queue duration for pool",
+		Buckets:     prometheus.LinearBuckets(0, 60, 20), // 40 buckets, each 30 seconds wide
+		ConstLabels: map[string]string{"pool": metricContext.pool.Name},
+	})
+
+	for _, job := range metricContext.finishedJobs {
+		queueTime := job.ReceiveTime.Sub(job.QueueTime) // Time received by the agent - Time queued by the user
+		queueTimes.Observe(queueTime.Seconds())
+	}
+
+	jobTimes := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:        "tfs_pool_job_running_length_secs",
+		Help:        "Total length of queue duration for pool",
+		Buckets:     prometheus.LinearBuckets(0, 60, 20), // 40 buckets, each 30 seconds wide
+		ConstLabels: map[string]string{"pool": metricContext.pool.Name},
+	})
+
+	for _, job := range metricContext.finishedJobs {
+		jobTime := job.FinishTime.Sub(job.ReceiveTime)
+		jobTimes.Observe(jobTime.Seconds())
+	}
+	return []prometheus.Metric{
+		totalTimes,
+		queueTimes,
+		jobTimes,
+	}
+}
+
 func calculateJobMetrics(metricContext metricsContext) []prometheus.Metric {
 
 	queuedTotal := 0
@@ -74,7 +118,10 @@ func calculateJobMetrics(metricContext metricsContext) []prometheus.Metric {
 			prometheus.GaugeValue,
 			float64(queuedTotal),
 			metricContext.pool.Name,
-		)}
+		),
+	}
+
+	calculatedMetrics = append(calculatedMetrics, calculateHistograms(metricContext)...)
 
 	return calculatedMetrics
 
